bc759f0ef03ad070a9460a1967be3114446d5666
CraftPig <crftpig@gmail.com>
2025-01-05 18:15:01 +0100

    k

--- lua/autorun/sh_stalker2_arc9.lua
+++ lua/autorun/sh_stalker2_arc9.lua
@@ -5,23 +5,9 @@ AddCSLuaFile()
 Subcategories
 -----------------------------------------------------------------------------------------------------
 ]]
--- list.Set("WeaponTreeIcons", "EFT", "logo16/logo_eftmedkit_16.png")
--- list.Set("WeaponTreeIcons", "EFT.Drugs", "logo16/logo_eftdrug_16.png")
--- list.Set("WeaponTreeIcons", "EFT.Stimulants", "logo16/logo_eftstim_16.png")
--- list.Set("WeaponTreeIcons", "EFT.Medkits", "logo16/logo_eftmedkit_16.png")
--- list.Set("WeaponTreeIcons", "EFT.Injury Treatment", "logo16/logo_eftinjury_16.png")
-
--- list.Set("WeaponTreeThumbnails", "EFT.Drugs", "entities/weapon_eft_augmentin.png")
--- list.Set("WeaponTreeThumbnails", "EFT.Stimulants", "entities/weapon_eft_injectoradrenaline.png")
--- list.Set("WeaponTreeThumbnails", "EFT.Medkits", "entities/weapon_eft_afak.png")
--- list.Set("WeaponTreeThumbnails", "EFT.Injury Treatment", "entities/weapon_eft_surgicalkit.png")
 
 --[[
 -----------------------------------------------------------------------------------------------------
 Sound Tables
 -----------------------------------------------------------------------------------------------------
 ]]
-
-
--- das
---
